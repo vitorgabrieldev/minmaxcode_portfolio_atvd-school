0fa71180ecf778b1a8ea1c3489cf0b0975071d95
vitorgabrieldev <vitorgabrieldeoliveiragit@gmail.com>
2023-09-04 15:53:29 -0300

    Cookies ajustes

--- home/app.js
+++ home/app.js
@@ -2,24 +2,16 @@ const cookieBanner = document.getElementById('cookie-banner');
 const aceitarCookieButton = document.getElementById('aceitar-cookie');
 const recusarCookieButton = document.getElementById('recusar-cookie');
 
+if (Cookies.get('cookieAccepted') == 'true') {
+  document.getElementById('cookie-banner').classList.add("hidden");
+}
+
 aceitarCookieButton.addEventListener('click', () => {
-    cookieBanner.style.display = 'none';
-    Toastify({
-        text: "Os Cookies foram aceitos!",
-        className: "success",
-        style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
-        }
-      }).showToast();
+  Cookies.set('cookieAccepted', 'true', { expires: 1 });
+  cookieBanner.style.display = 'none';
 });
 
 recusarCookieButton.addEventListener('click', () => {
-    cookieBanner.style.display = 'none';
-    Toastify({
-        text: "Os Cookies não foram aceitos!",
-        className: "success",
-        style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
-        }
-      }).showToast();
+  Cookies.set('cookieAccepted', 'false', { expires: 1 });
+  cookieBanner.style.display = 'none';
 });
